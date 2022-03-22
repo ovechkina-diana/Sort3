ba6dbf3e94a9fb78f8f0b2565acf709e1c68fdca
Diana Ovechkina <ovechkinadiana@yandex.ru>
2022-03-22 20:15:44 +0300

    Initial Commit

---
+++ Sort3/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Sort3
+//
+//  Created by Diana Ovechkina on 22.03.2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
